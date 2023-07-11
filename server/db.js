c12285bdc4a41bb9beb90adac2f06bd714fbe9c1
Suprabhat Sahoo <suprabhatbb62@gmail.com>
2023-07-11 22:21:14 +0530

    updated

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
-const Pool = require("pg").Pool;
+const { Pool } = require('pg');
 
-const pool = new Pool ({ 
-    user: "postgres",
-    password: "0800",
-    host: "localhost",
-    port: 5432,
-    database: "perntodo"
+const pool = new Pool({
+  user: 'perntodo_oau1_user',
+  password: 'P0iWZ3WmG2OF9enGM3RjyQmRDD2NjThI',
+  host: 'dpg-cilsvlp5rnuvtgr5i1r0-a',
+  port: 5432,
+  database: 'perntodo_oau1',
 });
 
 module.exports = pool;
